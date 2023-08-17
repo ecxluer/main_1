762625c950cf4d79e87ee43b24aed8466a9754b4
Bohdan Dereviaha <bohdan_dereviaha@epam.com>
2023-08-17 14:50:54 +0300

    Test

--- release.config.js
+++ release.config.js
@@ -1,10 +1,5 @@
 module.exports = {
     branches: ['main'], // Гілка, на якій ви хочете проводити версіонування
     plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/changelog',
-        '@semantic-release/npm',
-        '@semantic-release/github',
     ],
 };
