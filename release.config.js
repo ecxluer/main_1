ea9a1261fdee251c9d58ed8eddc4395f82c45eb0
Bohdan Dereviaha <bohdan_dereviaha@epam.com>
2023-08-17 16:01:01 +0300

    fix: виправлено помилку у коді

--- release.config.js
+++ release.config.js
@@ -4,12 +4,6 @@ module.exports = {
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
-        [
-            "@semantic-release/npm",
-            {
-                "npmPublish": false
-            }
-        ],
         '@semantic-release/github',
     ],
 };
